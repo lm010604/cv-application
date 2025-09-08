8887b5e5e610cb7d093fb1c8433b820a448cdce2
Lauren Mok <lm685@cornell.edu>
2025-09-07 23:38:45 -0400

    Moved svg files to src/assets and imported them in CustomButton

--- src/CustomButton.jsx
+++ src/CustomButton.jsx
@@ -1,12 +1,15 @@
+import editUrl from "/edit.svg";
+import deleteUrl from "/delete.svg";
+
 
 function CustomButton(props) {
     const btnClass = props.isEditIcon ? "edit-icon-btn" : (props.isDeleteIcon ? "delete-icon-btn" : "custom-btn");
     return (
         <button className={btnClass} onClick={props.handleClick}>
             {props.isEditIcon ? (
-                <img src="/edit.svg" alt="Edit" style={{ width: 15, height: 15, verticalAlign: "middle" }} />
+                <img src={editUrl} alt="Edit" style={{ width: 15, height: 15, verticalAlign: "middle" }} />
             ) : props.isDeleteIcon ? (
-                <img src="/delete.svg" alt="Delete" style={{ width: 15, height: 15, verticalAlign: "middle" }} />
+                <img src={deleteUrl} alt="Delete" style={{ width: 15, height: 15, verticalAlign: "middle" }} />
             ) : props.text}
         </button>
     )
